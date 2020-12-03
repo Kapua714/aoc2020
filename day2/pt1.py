95a7272b16a431ce9ea571e93961ade1fa1ea781
Leslie <lmeyers714@gmail.com>
2020-12-02 19:49:35 -0800

    AOC part 1 day 2

---
+++ day2/pt1.py
@@ -0,0 +1,13 @@
+f = open("day2_input.txt")
+print(f)
+alist = f.readlines()
+print(alist)
+numbers=[]
+for x in alist:
+    numbers.append(int(x))
+print(numbers)
+for x in numbers:
+    #x+y=2020 y=2020-x
+    y=2020-x
+    if y in numbers:
+        print(x*y)
